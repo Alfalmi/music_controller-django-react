ef015a650d56cef98db5ec93d2ad972d08c348f5
Alfalmi <pinooxd@gmail.com>
2021-09-03 01:53:01 -0700

    react components

--- api/views.py
+++ api/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
-from .models import Room
+from django.db import models
+import string
+import random
 
-# Create your views here.
 
-class RoomView(generics.ListAPIView):
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = "".join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Homepage from "./Homepage";
+
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    }
   }
 
   render() {
     return (
       <div>
-      React Hellow World
+      <Homepage />
       </div>
     );
   }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name='pino' />, appDiv);
